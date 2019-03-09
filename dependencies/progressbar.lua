5e1d12eafce529576893888d653975d2d5ec946c
l-7777777 <Lysander.mealy@gmail.com>
2019-03-09 15:53:51 -0500

    Update progressbar.lua

--- dependencies/progressbar.lua
+++ dependencies/progressbar.lua
@@ -1,7 +1,12 @@
 write("  ")
 term.setBackgroundColor(colors.gray)
 write("                                               ")
+local color = ...
 term.setCursorPos(3,2)
-term.setBackgroundColor(colors.blue)
+if color == "red" then
+  term.setBackgroundColor(colors.red)
+else
+  term.setBackgroundColor(colors.blue)
+end
 textutils.slowPrint("                                               ")
 term.setBackgroundColor(colors.black)
