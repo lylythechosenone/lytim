c00c4cd2ce4e602569efd4ea863cdb7e5179a9d6
l-7777777 <Lysander.mealy@gmail.com>
2019-03-10 16:49:58 -0400

    Create startup.lua

---
+++ startup.lua
@@ -0,0 +1,29 @@
+term.clear()
+term.setCursorPos(1,1)
+local main = http.get("")
+local mainf = fs.open("main", "w")
+mainf.write(main.readAll())
+mainf.close
+if pass == nil then
+  print("Choose a password.")
+  term.setBackgroundColor(colors.gray)
+  print("                             ")
+  local input = read("●")
+  local pass = input
+  print("Press any key to continue...")
+  os.pullEvent("key")
+else
+  while true do
+    print("Enter password")
+    term.setBackgroundColor(colors.gray)
+    print("                             ")
+    local input = read("●")
+    if input == pass then
+      shell.run("main")
+    else
+      print("Incorrect password.")
+      sleep(2)
+      shell.run("startup")
+    end
+  end
+end
