67b202de1e6c8538c9043b73ca2ac2e6eec31ade
l-7777777 <Lysander.mealy@gmail.com>
2019-03-10 13:40:52 -0400

    Update install.lua

--- install.lua
+++ install.lua
@@ -13,3 +13,11 @@ term.setCursorPos(9, 4)
 print("  OS  ")
 term.setCursorPos(9, 5)
 print("      ")
+local event, button, x, y = os.pullEvent("mouse_click")
+if (y > 2) and (y < 6) then
+  if x < 8 then
+    shell.run("store")
+  elseif (x > 8) and (x < 16) then
+    shell.run("installos")
+  end
+end
