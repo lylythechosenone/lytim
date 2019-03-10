1f950ee943ac9a79847ce2c55675102d3ac955b3
l-7777777 <Lysander.mealy@gmail.com>
2019-03-10 08:14:37 -0400

    Update update.lua

--- update.lua
+++ update.lua
@@ -1,21 +1,20 @@
-  local os = http.get("https://raw.githubusercontent.com/l-7777777/lytim/master/startup.lua")
-  local lo = fs.open("startup", "w")
-  if os.readAll() ~= lo.readAll() then
-    print("There is an update.")
-    print("Would you like to update this computer? [Y/n]")
-    local yorn = read()
-    if (yorn == Y) or (yorn == y) then
-      print("Restart to update")
-      term.setBackgroundColor(colors.green)
-      print("           ")
-      print("  Restart  ")
-      print("           ")
-      local event, button, x, y = os.pullEvent("mouse_click")
-      if (y < 7) and (x < 11) and (y > 4) then
-        os.reboot()
-      end
+local os = http.get("https://raw.githubusercontent.com/l-7777777/lytim/master/startup.lua")
+local lo = fs.open("startup", "w")
+if os.readAll() ~= lo.readAll() then
+  print("There is an update.")
+  print("Would you like to update this computer? [Y/n]")
+  local yorn = read()
+  if (yorn == Y) or (yorn == y) then
+    print("Restart to update")
+    term.setBackgroundColor(colors.green)
+    print("           ")
+    print("  Restart  ")
+    print("           ")
+    local event, button, x, y = os.pullEvent("mouse_click")
+    if (y < 7) and (x < 11) and (y > 4) then
+      os.reboot()
     end
-  else
-    print("There is no available update. If you belive this to be false, type pastebin run 3AVHyxWh to reinstall.")
   end
+else
+  print("There is no available update. If you belive this to be false, type pastebin run 3AVHyxWh to reinstall.")
 end
