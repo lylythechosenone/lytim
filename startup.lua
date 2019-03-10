2996ae6f3851d897e6da22b1b5bd029e72ff6671
l-7777777 <Lysander.mealy@gmail.com>
2019-03-10 17:09:37 -0400

    Update startup.lua

--- startup.lua
+++ startup.lua
@@ -3,7 +3,7 @@ term.setCursorPos(1,1)
 local main = http.get("https://raw.githubusercontent.com/l-7777777/lytim/DoorOS/main.lua")
 local mainf = fs.open("main", "w")
 mainf.write(main.readAll())
-mainf.close
+mainf.close()
 if pass == nil then
   print("Choose a password.")
   term.setBackgroundColor(colors.gray)
